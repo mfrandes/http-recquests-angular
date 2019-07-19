74d10b7eba0cedd255e6a8bd8a1d1170362b7aa4
mfrandes <frandesmyrce@gmail.com>
2019-07-19 19:17:44 +0300

    Showing a Loading Indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) { }
 
@@ -38,6 +39,7 @@ export class AppComponent implements OnInit {
     // Send Http request
   }
   private fetchPost() {
+    this.isFetching = true
     this.http
       .get
         <{[key: string]: Post}>('https://ng-complete-d55c5.firebaseio.com/posts.json')
@@ -52,6 +54,7 @@ export class AppComponent implements OnInit {
             }
             return postArray
           })).subscribe(posts => {
+            this.isFetching = false;
             this.loadedPosts = posts;
           })
   }
