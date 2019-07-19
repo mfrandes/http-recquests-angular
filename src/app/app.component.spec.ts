d168264f00da7d8ff2a93c77916710bc893af82f
mfrandes <frandesmyrce@gmail.com>
2019-07-19 18:35:43 +0300

    app init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'http-recquests-angular'`, () => {
+  it(`should have as title 'ng-complete-guide-update'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('http-recquests-angular');
+    expect(app.title).toEqual('ng-complete-guide-update');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to http-recquests-angular!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-complete-guide-update!');
   });
 });
