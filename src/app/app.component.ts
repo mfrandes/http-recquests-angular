2cec2a69ab330367e61a39e6ea193207cdf021c4
mfrandes <frandesmyrce@gmail.com>
2019-07-19 18:38:54 +0300

    bootstraps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent implements OnInit {
     // Send Http request
     this.http
       .post(
-        'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
+        'https://ng-complete-d55c5.firebaseio.com/',
         postData
       )
       .subscribe(responseData => {
